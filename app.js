cf93fc8a8e2bde25d4808f51c4ecfe689ebb2d5f
bhavyakaria <bhavyakaria@yahoo.in>
2021-01-28 01:59:41 +0530

    chore: connected mongo db successfully

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
 
 const app = express();
+app.use(express.json());
+
+// DB config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// Express Session 
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+// Routes
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on ${PORT}`));
