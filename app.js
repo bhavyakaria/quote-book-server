fbf9bed936d4151bc65824df7abe40311b2af702
bhavyakaria <bhavyakaria@yahoo.in>
2021-02-07 00:10:42 +0530

    build: 1.0.0

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const cors = require("cors");
 require('dotenv').config()
 
 const app = express();
 app.use(express.json());
 
 // cors middleware
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors());
 
 // Connect to Mongo
 mongoose.connect(process.env.MongoURI, { useNewUrlParser: true })
